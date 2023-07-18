118bf8ceecc8987fdd443bed67439dc094a6fc49
Carlos Eiras <carlos.eiras.franco@udc.es>
2023-07-18 13:13:38 +0200

    Revised scripts to generate new results

---
+++ src/2-measure-performance.py
@@ -0,0 +1,169 @@
+# %% [markdown]
+# # Measure performance
+# This notebook loads a file with precomputed measures (*qmeans*, *qbas* & *qinv*) for a set of rankings for a given instance of the dataset and measures the performance of the different alternative measures
+# 
+# ## 1. Load libraries, model and data
+
+# %%
+
+# Import the necessary libraries
+import sys
+import os
+PROJ_DIR = os.path.realpath(os.path.dirname(os.path.abspath('')))
+sys.path.append(os.path.join(PROJ_DIR,'src'))
+import xai_faithfulness_experiments_lib_edits as fl
+import numpy as np
+
+for FILENAME in os.listdir(os.path.join(PROJ_DIR,'results')):
+    if FILENAME.endswith('_measures.npz'):
+        print(FILENAME)
+
+        # Load data
+        data = fl.load_generated_data(os.path.join(PROJ_DIR, 'results', FILENAME))
+        qmeans = data['qmeans']
+        qmeans_basX = [data['qmean_bas']]
+        qmeans_inv = data['qmean_invs']
+
+        # Compute qmeans_bas[2-10]
+        def compute_qbas(measure, num_samples):
+            random_indices = np.random.randint(0,  measure.shape[0], (measure.shape[0], num_samples))
+            random_qmeans = measure[random_indices]
+            mean = np.mean(random_qmeans, axis=1)
+
+            # First way to deal with std==0; add some epsilon
+            #std = np.std(random_qmeans, axis=1) + 1e-10
+
+            # Second way to deal with std==0; ignore std (divide by 1)
+            std = np.std(random_qmeans, axis=1)
+            std[std==0] = 1
+
+            # Always ignore std
+            std=1
+            return (measure - mean) / std
+        for i in range(2,11):
+            qmeans_basX.append(compute_qbas(qmeans, i))
+
+        # Compute z-score??
+        qmean_mean = np.mean(qmeans)
+        qmean_std = np.std(qmeans)
+        z_scores = ((qmeans - qmean_mean) / qmean_std).flatten()
+
+        # Stratify z-index to be able to compare performance on different parts of the spectrum
+        indices = np.arange(z_scores.shape[0])
+        z_scores_numbered = np.vstack((z_scores, indices))
+        level_indices = []
+        boundaries = [float('-inf'), 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5]
+        for i in range(1,len(boundaries)+1):
+            bottom_limit = boundaries[i-1]
+            top_limit = float('inf')
+            if i < len(boundaries):
+                top_limit = boundaries[i]
+            level_indices.append((z_scores_numbered[:,np.logical_and(bottom_limit<=z_scores, z_scores<top_limit)][1,:].astype(int),(bottom_limit, top_limit)))
+
+        # %% [markdown]
+        # ## 2. Measure performance
+        # ### 2.1 Order preservation
+        #  1. The issue with using qmean directly is that it doesn't have a fixed scale and you don't get an idea of how good your explanation is compared to other explanations
+        #  2. To address this, ideally you would determine the distribution of all qmeans and then compute the z-score. That's very costly, so you either:
+        #     1. Estimate the qmeans distribution with X samples $\rightarrow$ qbasX
+        #     2. Calculate an alternative to the z-index directly $\rightarrow$ qinv
+        #  3. The problem with both alternatives is that you adulterate the value of your original qmean measurement, so you may end up in a situation where $qmean_i<qmean_j$ but $qinv_i<qinv_j$, which is undesirable
+        #  4. Hence, we measure how many times that happens for each measure.
+        # 
+        #  (This may be measuring the same as Pearson correlation, which is computed below)
+
+        # %%
+        def measure_correct_orderings(truths, estimators):
+            '''
+            Creates len(truth) x,y pairs and computes the fraction of them for which (truths[x]<truths[y] and estimators[x]<estimators[y]) or (truths[x]>truths[y] and estimators[x]>estimators[y])
+            Inputs:
+                - Truths & estimators contain num_elems floats
+            Output:
+                - Float representing the fraction of correctly ordered pairings
+            '''
+            xs = np.random.permutation(truths.size)
+            ys = np.random.permutation(truths.size)
+            truthX_lt_Y = truths[xs] < truths[ys]
+            estimatorX_lt_Y = estimators[xs] < estimators[ys]
+            hits = truthX_lt_Y==estimatorX_lt_Y
+            return hits.sum()/truths.size
+
+        correct_pairings_basX = []
+        for i in range(len(qmeans_basX)):
+            correct_pairings_basX.append(measure_correct_orderings(qmeans, qmeans_basX[i]))
+            print(f'qmeans_bas{i+1}: {correct_pairings_basX[i]:.4f}')
+        correct_pairings_inv = measure_correct_orderings(qmeans, qmeans_inv)
+        print(f'qmeans_inv: {correct_pairings_inv:.4f}')
+
+        # %% [markdown]
+        # ### 2.2. Spearman correlation
+        # Same thing, is the order of qmeans preserved in qbasX/qinv?
+
+        # %%
+        from scipy.stats import spearmanr
+        spearman_basX = []
+        for i in range(len(qmeans_basX)):
+            spearman_basX.append(spearmanr(qmeans, qmeans_basX[i])[0])
+            print(f'qmeans_bas{i+1}: {spearman_basX[i]:.4f}')
+        spearman_inv = spearmanr(qmeans, qmeans_inv)[0]
+        print(f'qmeans_inv: {spearman_inv:.4f}')
+
+        # %% [markdown]
+        # ### 2.3. Ability to detect exceptionally good rankings
+        # As stated above, there are some ordering errors in the estimators. Are they in the relevant part of the distribution? i.e. Do they affect the ability to identify exceptionally good rankings?
+
+        # %%
+        from sklearn import metrics
+
+        def measure_detection(target_indices, estimator):
+            if len(target_indices)==0:
+                return 1
+            target = np.zeros_like(estimator, dtype=int)
+            target[target_indices] = 1
+            return metrics.roc_auc_score(target, estimator)
+
+        aucs_inv = []
+        aucs_basX = [[] for i in qmeans_basX]
+
+        for indices, (bottom_limit, upper_limit) in level_indices[2:]:
+            aucs_inv.append(measure_detection(indices, qmeans_inv))
+            for i in range(len(qmeans_basX)):
+                aucs_basX[i].append(measure_detection(indices, qmeans_basX[i]))
+
+        for i in range(len(qmeans_basX)):
+            print(f'aucs_bas{i} ' + ' | '.join(map(lambda x: f'{x:.4f}',aucs_basX[i])))
+        print('aucs_inv ' + ' | '.join(map(lambda x: f'{x:.4f}',aucs_inv)))
+
+
+        # %% [markdown]
+        # ### 2.4 Ability to rank exceptionally good rankings
+        # How well is the order preserved for exceptionally good rankings?
+
+        # %%
+        spearman_exceptional_inv = []
+        spearman_exceptional_basX = [[] for i in qmeans_basX]
+
+        for indices, (bottom_limit, upper_limit) in level_indices[2:]:
+            spearman_exceptional_inv.append(spearmanr(qmeans[indices], qmeans_inv[indices])[0])
+            for i in range(len(qmeans_basX)):
+                spearman_exceptional_basX[i].append(spearmanr(qmeans[indices], qmeans_basX[i][indices])[0])
+
+        for i in range(len(qmeans_basX)):
+            print(f'spearman_exceptional_bas{i} ' + ' | '.join(map(lambda x: f'{x:.4f}', spearman_exceptional_basX[i])))
+        print('spearman_exceptional_inv ' + ' | '.join(map(lambda x: f'{x:.4f}', spearman_exceptional_inv)))
+
+        # %% [markdown]
+        # ### 3. Save
+
+        # %%
+        np.savez(os.path.join(PROJ_DIR, 'results', FILENAME.replace('_measures','_results')), \
+                correct_pairings_inv=correct_pairings_inv, \
+                correct_pairings_basX=correct_pairings_basX, \
+                spearman_inv=spearman_inv, \
+                spearman_basX=spearman_basX, \
+                aucs_inv=aucs_inv, \
+                aucs_basX=aucs_basX, \
+                spearman_exceptional_inv=spearman_exceptional_inv, \
+                spearman_exceptional_basX=spearman_exceptional_basX)
+
+
--- src/extract_tabular_measures.py
+++ src/extract_tabular_measures.py
@@ -65,122 +65,124 @@ all_rankings = np.array(permutations) / (NUM_VARS - 1)
 from tqdm import tqdm
 import quantus
 
-SAMPLE_NUM = 70 # Select one of the training examples in the dataset to be explained
+for SAMPLE_NUM in [10, 20, 30, 40, 60, 80, 90, 100, 150, 300]:
+    #SAMPLE_NUM = 70 # Select one of the training examples in the dataset to be explained
+    print('Processing', SAMPLE_NUM)
 
-num_rankings = all_rankings.shape[0]
-row = torch.tensor(np.float32(x_train[SAMPLE_NUM])).to(device)
-label = torch.tensor(y_train[SAMPLE_NUM]).to(device)
+    num_rankings = all_rankings.shape[0]
+    row = torch.tensor(np.float32(x_train[SAMPLE_NUM])).to(device)
+    label = torch.tensor(y_train[SAMPLE_NUM]).to(device)
 
-# All of these measures will be stored
-suffixes = ['', '_inv', '_bas']
-size1_prefixes = ['mean', 'at_first_argmax', 'auc']
-sizeNUM_SAMPLES_prefixes = ['output_curve', 'is_hit_curve']
-keys = ['ranking']
-for p in size1_prefixes+sizeNUM_SAMPLES_prefixes:
-    for s in suffixes:
-        keys.append(p+s)
+    # All of these measures will be stored
+    suffixes = ['', '_inv', '_bas']
+    size1_prefixes = ['mean', 'at_first_argmax', 'auc']
+    sizeNUM_SAMPLES_prefixes = ['output_curve', 'is_hit_curve']
+    keys = ['ranking']
+    for p in size1_prefixes+sizeNUM_SAMPLES_prefixes:
+        for s in suffixes:
+            keys.append(p+s)
 
-# Dict to store all results
-all_measures = {}
-# Initialize all np arrays to speed up the process
-for k in size1_prefixes:
-    for s in suffixes:
-        all_measures[k+s] = np.zeros((num_rankings, 1), dtype=np.float32)
+    # Dict to store all results
+    all_measures = {}
+    # Initialize all np arrays to speed up the process
+    for k in size1_prefixes:
+        for s in suffixes:
+            all_measures[k+s] = np.zeros((num_rankings, 1), dtype=np.float32)
 
-for k in sizeNUM_SAMPLES_prefixes:
-    for s in suffixes:
-        all_measures[k+s] = np.zeros((num_rankings, fl.NUM_SAMPLES), dtype=np.float32 if 'is_hit' in k else bool)
-all_measures['ranking'] = np.zeros((num_rankings, NUM_VARS), dtype=np.float32)
+    for k in sizeNUM_SAMPLES_prefixes:
+        for s in suffixes:
+            all_measures[k+s] = np.zeros((num_rankings, fl.NUM_SAMPLES), dtype=np.float32 if 'is_hit' in k else bool)
+    all_measures['ranking'] = np.zeros((num_rankings, NUM_VARS), dtype=np.float32)
 
-# Compute the results for each possible ranking
-for i in tqdm(range(num_rankings), miniters=1000):
-    #TODO - Add several samples for qbas instead of a single one
-    measures = fl.get_measures_for_ranking(row, torch.tensor(all_rankings[i]).to(device), label, network, num_samples=fl.NUM_SAMPLES, with_inverse=True, with_random=True)
-    measures['ranking'] = all_rankings[i]
-    # Save all results for this rankings to the i-th position
-    for k in keys:
-        all_measures[k][i] = measures[k]
+    # Compute the results for each possible ranking
+    for i in tqdm(range(num_rankings), miniters=1000):
+        #TODO - Add several samples for qbas instead of a single one
+        measures = fl.get_measures_for_ranking(row, torch.tensor(all_rankings[i]).to(device), label, network, num_samples=fl.NUM_SAMPLES, with_inverse=True, with_random=True)
+        measures['ranking'] = all_rankings[i]
+        # Save all results for this rankings to the i-th position
+        for k in keys:
+            all_measures[k][i] = measures[k]
 
 
-#Retrieve and store Quantus' faithfulness metrics
-# To be used by Quantus
-x_batch_pt = torch.unsqueeze(torch.unsqueeze(torch.unsqueeze(row, dim=0), dim=0), dim=0)
-x_batch = x_batch_pt.to('cpu').numpy()
-y_batch = torch.unsqueeze(label, dim=0).to('cpu').numpy()
+    #Retrieve and store Quantus' faithfulness metrics
+    # To be used by Quantus
+    x_batch_pt = torch.unsqueeze(torch.unsqueeze(torch.unsqueeze(row, dim=0), dim=0), dim=0)
+    x_batch = x_batch_pt.to('cpu').numpy()
+    y_batch = torch.unsqueeze(label, dim=0).to('cpu').numpy()
 
-all_measures['faithfulness_correlation'] = np.zeros(num_rankings, dtype=np.float32)
-all_measures['monotonicity_correlation'] = np.zeros(num_rankings, dtype=np.float32)
-all_measures['pixel_flipping'] = np.zeros((num_rankings,NUM_VARS), dtype=np.float32)
+    all_measures['faithfulness_correlation'] = np.zeros(num_rankings, dtype=np.float32)
+    all_measures['monotonicity_correlation'] = np.zeros(num_rankings, dtype=np.float32)
+    all_measures['pixel_flipping'] = np.zeros((num_rankings,NUM_VARS), dtype=np.float32)
 
-for i in tqdm(range(num_rankings),  miniters=1000):
-    #For each ranking, retrieve and store Quantus' faithfulness metrics
-    a_batch = np.expand_dims(np.expand_dims(np.expand_dims(all_rankings[i],0),0),0)
-    #print('x_batch shape:',x_batch.shape)
-    #print('y_batch shape:',y_batch.shape)
-    #print('a_batch shape:',a_batch.shape)
-    #print('network(x_batch) shape:',network(torch.tensor(x_batch).to(device)).shape)
-    #print(a_batch)
-    all_measures['faithfulness_correlation'][i] = quantus.FaithfulnessCorrelation(
-                                                    nr_runs=10,
-                                                    subset_size=4,  
-                                                    perturb_baseline="black",
-                                                    perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
-                                                    similarity_func=quantus.similarity_func.correlation_pearson,  
-                                                    abs=False,  
-                                                    return_aggregate=False,
-                                                    disable_warnings=True
-                                                )(model=network, 
-                                                x_batch=x_batch, 
-                                                y_batch=y_batch,
-                                                a_batch=a_batch,
-                                                device=device,
-                                                channel_first=True)[0]
-    all_measures['monotonicity_correlation'][i] = quantus.MonotonicityCorrelation(
-                                                    nr_samples=10,
-                                                    features_in_step=2,
-                                                    perturb_baseline="black",
-                                                    perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
-                                                    similarity_func=quantus.similarity_func.correlation_spearman,
-                                                    disable_warnings=True
-                                                )(model=network, 
-                                                   x_batch=x_batch,
-                                                   y_batch=y_batch,
-                                                   a_batch=a_batch,
-                                                   device=device,
-                                                   channel_first=True)[0]
-    all_measures['pixel_flipping'][i] = quantus.PixelFlipping(
-                                                    features_in_step=1,
-                                                    perturb_baseline="black",
-                                                    perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
-                                                    disable_warnings=True
-                                                )(model=network,
+    for i in tqdm(range(num_rankings),  miniters=1000):
+        #For each ranking, retrieve and store Quantus' faithfulness metrics
+        a_batch = np.expand_dims(np.expand_dims(np.expand_dims(all_rankings[i],0),0),0)
+        #print('x_batch shape:',x_batch.shape)
+        #print('y_batch shape:',y_batch.shape)
+        #print('a_batch shape:',a_batch.shape)
+        #print('network(x_batch) shape:',network(torch.tensor(x_batch).to(device)).shape)
+        #print(a_batch)
+        all_measures['faithfulness_correlation'][i] = quantus.FaithfulnessCorrelation(
+                                                        nr_runs=10,
+                                                        subset_size=4,  
+                                                        perturb_baseline="black",
+                                                        perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+                                                        similarity_func=quantus.similarity_func.correlation_pearson,  
+                                                        abs=False,  
+                                                        return_aggregate=False,
+                                                        disable_warnings=True
+                                                    )(model=network, 
+                                                    x_batch=x_batch, 
+                                                    y_batch=y_batch,
+                                                    a_batch=a_batch,
+                                                    device=device,
+                                                    channel_first=True)[0]
+        all_measures['monotonicity_correlation'][i] = quantus.MonotonicityCorrelation(
+                                                        nr_samples=10,
+                                                        features_in_step=2,
+                                                        perturb_baseline="black",
+                                                        perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+                                                        similarity_func=quantus.similarity_func.correlation_spearman,
+                                                        disable_warnings=True
+                                                    )(model=network, 
                                                     x_batch=x_batch,
                                                     y_batch=y_batch,
                                                     a_batch=a_batch,
                                                     device=device,
-                                                   channel_first=True)[0]
+                                                    channel_first=True)[0]
+        all_measures['pixel_flipping'][i] = quantus.PixelFlipping(
+                                                        features_in_step=1,
+                                                        perturb_baseline="black",
+                                                        perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+                                                        disable_warnings=True
+                                                    )(model=network,
+                                                        x_batch=x_batch,
+                                                        y_batch=y_batch,
+                                                        a_batch=a_batch,
+                                                        device=device,
+                                                    channel_first=True)[0]
 
 
 
-np.savez(os.path.join(PROJ_DIR, 'results', f'{DATASET}_{SAMPLE_NUM}_measures.npz'), \
-         row=row.to('cpu').numpy(), \
-         label=label.to('cpu').numpy(), \
-         rankings=all_measures['ranking'], \
-         faithfulness_correlations=all_measures['faithfulness_correlation'], \
-         monotonicity_correlations=all_measures['monotonicity_correlation'], \
-         pixel_flippings=all_measures['pixel_flipping'], \
-         qmeans=all_measures['mean'], \
-         qmean_invs=all_measures['mean_inv'], \
-         qmean_bas=all_measures['mean_bas'], \
-         qargmaxs=all_measures['at_first_argmax'], \
-         qargmax_invs=all_measures['at_first_argmax_inv'], \
-         qargmax_bas=all_measures['at_first_argmax_bas'], \
-         qaucs=all_measures['auc'], \
-         qauc_invs=all_measures['auc_inv'], \
-         qauc_bas=all_measures['auc_bas'], \
-         output_curves=all_measures['output_curve'], \
-         is_hit_curves=all_measures['is_hit_curve'], \
-         output_curves_inv=all_measures['output_curve_inv'], \
-         is_hit_curves_inv=all_measures['is_hit_curve_inv'], \
-         output_curves_bas=all_measures['output_curve_bas'], \
-         is_hit_curves_bas=all_measures['is_hit_curve_bas'])
+    np.savez(os.path.join(PROJ_DIR, 'results', f'{DATASET}_{SAMPLE_NUM}_measures.npz'), \
+            row=row.to('cpu').numpy(), \
+            label=label.to('cpu').numpy(), \
+            rankings=all_measures['ranking'], \
+            faithfulness_correlations=all_measures['faithfulness_correlation'], \
+            monotonicity_correlations=all_measures['monotonicity_correlation'], \
+            pixel_flippings=all_measures['pixel_flipping'], \
+            qmeans=all_measures['mean'], \
+            qmean_invs=all_measures['mean_inv'], \
+            qmean_bas=all_measures['mean_bas'], \
+            qargmaxs=all_measures['at_first_argmax'], \
+            qargmax_invs=all_measures['at_first_argmax_inv'], \
+            qargmax_bas=all_measures['at_first_argmax_bas'], \
+            qaucs=all_measures['auc'], \
+            qauc_invs=all_measures['auc_inv'], \
+            qauc_bas=all_measures['auc_bas'], \
+            output_curves=all_measures['output_curve'], \
+            is_hit_curves=all_measures['is_hit_curve'], \
+            output_curves_inv=all_measures['output_curve_inv'], \
+            is_hit_curves_inv=all_measures['is_hit_curve_inv'], \
+            output_curves_bas=all_measures['output_curve_bas'], \
+            is_hit_curves_bas=all_measures['is_hit_curve_bas'])
