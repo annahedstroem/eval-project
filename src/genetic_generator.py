220e8797cbafe547b5a65e5599f722587f14b9e7
Carlos Eiras <carlos.eiras.franco@udc.es>
2023-10-20 19:42:18 +0200

    Added fresh elements in genetic generation

--- src/genetic_generator.py
+++ src/genetic_generator.py
@@ -29,26 +29,29 @@ def _spawn_ranking(p1:np.ndarray[int], p2:np.ndarray[int]) -> np.ndarray[int]:
         new_r[i] = candidates.pop(random.randint(0,len(candidates)-1))
     return new_r
 
-def _repopulate(population:np.ndarray[int], num_saved:int) -> np.ndarray[int]:
+def _repopulate(population:np.ndarray[int], num_saved:int, random_elements:int = 0) -> np.ndarray[int]:
     '''
     Trims a population to its top `num_saved` performers and repopulates the rest by performing random crossings of the remaining elements
 
     Population must be sorted across axis 0 in descending order of performance
     '''
     rng = default_rng()
-    for i in range(num_saved, population.shape[0]): # Loop through all non-top performers to overwrite them
-       # Select two random parents from the num_saved top performers of the population
-       parent_indices = rng.permutation(num_saved)[:2]
-       parent1 = population[parent_indices[0]]
-       parent2 = population[parent_indices[1]]
-       # Generate a ranking and add it to the population (overwriting one of the poor performers)
-       population[i] = _spawn_ranking(parent1, parent2)
+    for i in range(num_saved, population.shape[0] - random_elements): # Loop through all non-top performers to overwrite them
+        # Select two random parents from the num_saved top performers of the population
+        parent_indices = rng.permutation(num_saved)[:2]
+        parent1 = population[parent_indices[0]]
+        parent2 = population[parent_indices[1]]
+        # Generate a ranking and add it to the population (overwriting one of the poor performers)
+        population[i] = _spawn_ranking(parent1, parent2)
+    for i in range(random_elements):
+        population[-random_elements + i] = rng.permutation(population.shape[1])
 
 
 def generate_rankings(num_rankings: int,\
                       element_shape: tuple[int],\
                       fitness_function: Callable[[np.ndarray[int]], float],\
                       population_size: Optional[int] = None,\
+                      new_random_elements_per_iteration:int =0,\
                       num_iterations:int = 10):
     '''
     Generates with genetic programming as many rankings as indicated by num_rankings
@@ -66,14 +69,14 @@ def generate_rankings(num_rankings: int,\
        num_features *= d
 
     # Generate an initial population of entirely random rankings
-    population = np.zeros((population_size, num_features), dtype=int)
+    population = np.zeros((population_size + new_random_elements_per_iteration, num_features), dtype=int)
     rng = default_rng()
-    for i in range(population_size):
+    for i in range(population_size + new_random_elements_per_iteration):
         population[i] = rng.permutation(num_features)
 
     for i in range(num_iterations):
         iteration_fitnesses = []
-        for j in tqdm(range(population_size)):
+        for j in tqdm(range(population_size + new_random_elements_per_iteration)):
             fitness = fitness_function(population[j].reshape(element_shape) / (num_features - 1))
             iteration_fitnesses.append((fitness, j))
         iteration_fitnesses.sort(reverse=True)
@@ -82,7 +85,8 @@ def generate_rankings(num_rankings: int,\
         population = population[sorted_indices]
         avg_fitness = np.mean(list(map(lambda x:x[0], iteration_fitnesses)))
         print(f'{i+1}/{num_iterations} - Avg. fitness {avg_fitness}')
-        _repopulate(population, num_rankings)
+        if i < num_iterations - 1: # Don't do this in the last iteration
+            _repopulate(population, num_rankings, new_random_elements_per_iteration)
     
     return population[:num_rankings].reshape((num_rankings, *element_shape)) / (num_features - 1)
 
