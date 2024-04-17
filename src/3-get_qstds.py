34b81d3090abfba8ae9e0b3c165ece49f1127cfd
Carlos Eiras <carlos.eiras.franco@udc.es>
2024-04-17 11:35:34 +0200

    Code for generating tables in the paper.

--- src/3-get_qstds.py
+++ src/3-get_qstds.py
@@ -3,7 +3,7 @@ import os
 
 PROJ_DIR = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
 
-keys = ['correct_pairings_inv', 'correct_pairings_basX', 'spearman_inv', 'spearman_basX', 'aucs_inv', 'aucs_basX', 'spearman_exceptional_inv', 'spearman_exceptional_basX']
+keys = ['qmeans']
 
 tuples_to_test = [('imagenet', 'resnet18'), \
                       #('imagenet', 'resnet50'), \
@@ -51,26 +51,20 @@ SUFFIX = '_EfficientMPRT'
 # SUFFIX = ''
 
 # TABLE5
-# tuples_to_test = [('avila', '0'), \
-#                       ('avila', 'ood-mean'), \
-#                       ('avila', 'ood-zeros'), \
-#                       ('avila', 'undertrained'), \
-#                       ('avila', 'untrained'), \
-#                       ('glass', '0'), \
-#                       ('glass', 'ood-mean'), \
-#                       ('glass', 'ood-zeros'), \
-#                       ('glass', 'undertrained'), \
-#                       ('glass', 'untrained')
-#                     ]
-# target_measures = ['']
-# GENERATION_MODE = ''
-# SUFFIX = ''
-
-# TABLE6
-tuples_to_test = [('cmnist', 'resnet18')]
-target_measures = ['AttributionLocalisation', 'TopKIntersection', 'RelevanceRankAccuracy', 'RelevanceMassAccuracy', 'AUC']
-GENERATION_MODE = '_randomattr'
-SUFFIX = '_localization_s_area'
+tuples_to_test = [('avila', '0'), \
+                      ('avila', 'ood-mean'), \
+                      ('avila', 'ood-zeros'), \
+                      ('avila', 'undertrained'), \
+                      ('avila', 'untrained'), \
+                      ('glass', '0'), \
+                      ('glass', 'ood-mean'), \
+                      ('glass', 'ood-zeros'), \
+                      ('glass', 'undertrained'), \
+                      ('glass', 'untrained')
+                    ]
+target_measures = ['']
+GENERATION_MODE = ''
+SUFFIX = ''
 
 print(f'Dataset - Model\tDeltaPA\tDeltaRho')
 
@@ -86,7 +80,7 @@ for TARGET_MEASURE in target_measures:
         num_files = 0 # Count how many files are involved for use below
         result_dict = {}
         for f in os.listdir(os.path.join(PROJ_DIR, 'results')):
-            if f.startswith(DATASET_NAME) and f.endswith(f"{MODEL_NAME}{GENERATION_MODE}{SUFFIX}_results{'_' + TARGET_MEASURE if TARGET_MEASURE != '' else ''}.npz"):#{MODEL_NAME}{GENERATION_MODE}_localization_s_area_results{TARGET_MEASURE}.npz
+            if f.startswith(DATASET_NAME) and f.endswith(f'{MODEL_NAME}{GENERATION_MODE}{SUFFIX}_measures{TARGET_MEASURE}.npz'):#{MODEL_NAME}{GENERATION_MODE}_localization_s_area_results{TARGET_MEASURE}.npz
                 FILENAME = os.path.join(PROJ_DIR, 'results', f)
                 
                 num_files += 1
@@ -100,20 +94,7 @@ for TARGET_MEASURE in target_measures:
         assert num_files>0, f'No files for {DATASET_NAME} {MODEL_NAME}{GENERATION_MODE} {TARGET_MEASURE}'
         
         for k in keys:
-            result_dict[f'{k}_mean'] = np.nanmean(result_dict[k], axis=0)
-            result_dict[f'{k}_std'] = np.nanstd(result_dict[k], axis=0)
+            result_dict[f'{k}_mean'] = np.nanmean(result_dict[k], axis=1)
+            result_dict[f'{k}_std'] = np.nanstd(result_dict[k], axis=1)
         
-        delta_p = result_dict['correct_pairings_inv_mean']-result_dict['correct_pairings_basX_mean'][0]
-        #print('delta_p', delta_p)
-
-        delta_ps = result_dict['correct_pairings_inv']-result_dict['correct_pairings_basX'][:,0]
-        delta_p_mean = delta_ps.mean()
-        delta_p_std = delta_ps.std()
-
-        delta_rho = result_dict['spearman_inv_mean']-result_dict['spearman_basX_mean'][0]
-        delta_rhos = result_dict['spearman_inv']-result_dict['spearman_basX'][:,0]
-        delta_rho_mean = delta_rhos.mean()
-        delta_rho_std = delta_rhos.std()
-        #print('delta_rho', delta_rho)
-        #print(f'{DATASET_NAME} & {MODEL_NAME} & {delta_p[0]:.3f} & {delta_rho[0]:.3f}\\\\')
-        print(f'{DATASET_NAME} & {MODEL_NAME} & {delta_p_mean:.3f}$\\pm${delta_p_std:.3f} & {delta_rho_mean:.3f}$\\pm${delta_rho_std:.3f}\\\\')
+        print(f'{DATASET_NAME} & {MODEL_NAME} & {result_dict["qmeans_std"].mean():.3f}$\\pm${result_dict["qmeans_std"].std():.3f}\\\\')
